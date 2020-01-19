7d3b422aa1088b9de8af4e84a485d13f31b37339
Oluwakayode <fadojuk@gmail.com>
2020-01-20 00:49:52 +0100

    Updates

---
+++ precache-manifest.3dbee464c3c04199d81c1bf6cf448757.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6a4ea5a3242f63b1f6745afbd1a6988",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "d6c16526bd7b491c4f87",
+    "url": "/react-todo-app/static/css/main.3bed6c01.chunk.css"
+  },
+  {
+    "revision": "7b89c00ee92549ce4de7",
+    "url": "/react-todo-app/static/js/2.0fabfd10.chunk.js"
+  },
+  {
+    "revision": "1eed39b55c0bb7c26ec629a63f87aa2a",
+    "url": "/react-todo-app/static/js/2.0fabfd10.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d6c16526bd7b491c4f87",
+    "url": "/react-todo-app/static/js/main.2af8b3b3.chunk.js"
+  },
+  {
+    "revision": "0e0ffa60d2a6a6983e78",
+    "url": "/react-todo-app/static/js/runtime-main.f96e123f.js"
+  }
+]);
