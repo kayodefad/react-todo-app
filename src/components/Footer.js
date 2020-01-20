704f95e0081952ccfdcaa6e5aa72922c76a99f8f
Oluwakayode <fadojuk@gmail.com>
2020-01-20 07:38:48 +0100

    Added link to github

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,16 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className="fixed-bottom bg-dark text-light text-center p-2">
-      Copyright &copy; 2020 kayodeFad
+      Copyright &copy; 2020 kayodeFad{' '}
+      <a
+        style={{ color: '#fff' }}
+        href="https://github.com/kayodefad"
+        target="_blank"
+      >
+        <i className="fab fa-github"></i>
+      </a>
     </div>
-  );
+  )
 }
 
-export default Footer;
+export default Footer
